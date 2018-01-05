42bd0f8823992039124a8649651e630222377379
Shweta Chadha <schadha@sapient.com>
2018-01-05 22:39:01 +0530

    Removed the initial java application.

--- src/main/java/com/techgig/fulfillment/services/FulfillmentService.java
+++ src/main/java/com/techgig/fulfillment/services/FulfillmentService.java
@@ -15,7 +15,7 @@ public class FulfillmentService {
 	
 	public List<Consignment> fulfill(List<Order> orders, List<Warehouse> warehouses) {
 		List<Consignment> fulfillmentDataList = new ArrayList<>();
-		for (Order order : orders) {
+		orders.forEach(order -> {
 			//Calculate fitness of warehouses per order
 			calculateFitness(order, warehouses);
 
@@ -24,58 +24,53 @@ public class FulfillmentService {
 
 			//Fill the orders
 			fulfillmentDataList.addAll(fillOrder(order, warehouses));
-		}
+		});
+
 		return fulfillmentDataList;
 	}
 
 	private List<Consignment> fillOrder(Order order, List<Warehouse> warehouses) {
 		List<Consignment> consignments = new ArrayList<>();
 		Map<String, Double> orderEntries = order.getOrderEntries();
-		for (Warehouse warehouse : warehouses) {
+		warehouses.forEach(warehouse -> {
 			Map<String, Double> inventory = warehouse.getInventory();
-			
-			for (Map.Entry<String, Double> orderEntry : orderEntries.entrySet()) {
-				Double amountFulfilled = 0.0;
-				String sku = orderEntry.getKey();
-				Double orderedQty = orderEntry.getValue();
-				
-				if (orderedQty == 0.0) {
-					continue;
-				}
-				
-				if (inventory.get(sku) != null) {
-				
-					Double qtyInInventory = inventory.get(sku);
-					
-					//Reduce the quantity in order and calculate the amountFulfilled
-					if (orderedQty <= qtyInInventory) {
-						orderEntry.setValue(0.0);
-						amountFulfilled = orderedQty;
-					} else {
-						orderEntry.setValue(orderedQty - qtyInInventory);
-						amountFulfilled = qtyInInventory;
-					}
-					
-					//Reduce the inventory quantity
-					qtyInInventory = qtyInInventory - orderedQty;
-					inventory.put(sku, qtyInInventory);
-						
-					//Populate the output data structure
-					Consignment consignment = new Consignment(warehouse.getLocation(), order.getDestination(), sku, amountFulfilled);
-					consignments.add(consignment);
-				}
-				
-			}
-			
-		}
+			orderEntries.entrySet().stream()
+					.filter(orderEntry -> orderEntry.getValue() != 0.0)
+					.forEach(orderEntry -> {
+						Double amountFulfilled;
+						String sku = orderEntry.getKey();
+						Double orderedQty = orderEntry.getValue();
+
+						if (inventory.get(sku) != null) {
+							Double qtyInInventory = inventory.get(sku);
+
+							//Reduce the quantity in order and calculate the amountFulfilled
+							if (orderedQty <= qtyInInventory) {
+								orderEntry.setValue(0.0);
+								amountFulfilled = orderedQty;
+							} else {
+								orderEntry.setValue(orderedQty - qtyInInventory);
+								amountFulfilled = qtyInInventory;
+							}
+
+							//Reduce the inventory quantity
+							qtyInInventory = qtyInInventory - orderedQty;
+							inventory.put(sku, qtyInInventory);
+
+							//Populate the output data structure
+							Consignment consignment = new Consignment(warehouse.getLocation(), order.getDestination(), sku, amountFulfilled);
+							consignments.add(consignment);
+						}
+					});
+		});
 		return consignments;
 	}
 
 	private void calculateFitness(Order order, List<Warehouse> warehouses) {
-		for (Warehouse warehouse : warehouses) {
+		warehouses.forEach(warehouse -> {
 			double percentFulfilled = calculateFitnessOfWarehouseForOrder(order, warehouse);
 			warehouse.setPercentFulfilled(percentFulfilled);
-		}		
+		});
 	}
 	
 	private double calculateFitnessOfWarehouseForOrder(Order order, Warehouse warehouse) {
@@ -84,19 +79,19 @@ public class FulfillmentService {
 		
 		double quantityNotFulfilled = 0;
 		double quantityOfAllItemsInOrder = 0;
-		double percentFulfilled = 0;
-		
+		double percentFulfilled;
+
 		for (Map.Entry<String, Double> entryForOrderEntries : orderEntries.entrySet()) {
 			double quantityInOrder = entryForOrderEntries.getValue();
 			double quantityInWarehouse = inventory.get(entryForOrderEntries.getKey()) == null ? 0 : inventory.get(entryForOrderEntries.getKey());
-			
-			quantityOfAllItemsInOrder = quantityOfAllItemsInOrder + quantityInOrder;
-			
+
+			quantityOfAllItemsInOrder += quantityInOrder;
+
 			if (quantityInWarehouse < quantityInOrder) {
 				quantityNotFulfilled = quantityNotFulfilled + (quantityInOrder - quantityInWarehouse);
-			}			
+			}
 		}
-		
+
 		if (quantityNotFulfilled > 0) {
 			percentFulfilled = (quantityOfAllItemsInOrder - quantityNotFulfilled)/quantityOfAllItemsInOrder;
 		} else {
